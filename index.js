956a955d5638ea958a408ad89e9f3f035006657f
CoreyBabs <coreymbabcock@gmail.com>
2023-07-05 23:07:36 -0600

    add size button and resizing of grid

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ function createGrid(dim) {
 	let [vw, vh] = getViewportSize();
 	let sw = Math.floor(vw / dim);
 	let sh = Math.floor(vh / dim);
-	console.log(vh);
-	console.log(sh);
 	for (let i = 0; i < dim * dim; i++) {
 		let div = document.createElement("div");
 		div.classList.add("square");
@@ -37,5 +35,22 @@ function getViewportSize() {
 	const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
 	return [vw, vh];
 }
+
+function promptForSize() {
+	let result = prompt("Enter grid size. Default is 16, max is 100");
+	let size = +result; // convert result to int, or NaN if it fails
+
+	if (result === null || isNaN(size) || size === 0) {
+		return;
+	}
+
+	let dim = Math.min(size, 100);
+	resetGrid();
+	createGrid(dim);
+}
+
 sizeContainer();
 createGrid(16);
+
+let sizeBtn = document.querySelector("#size");
+sizeBtn.addEventListener("click", promptForSize);
